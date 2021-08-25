f7e9234542f988dab0dd8e0d28020ee28abf3d00
veena.g <veena.g@technovert.com>
2021-08-25 13:37:23 +0530

    test env details for deployment

--- src/JSONFormMetadata/OrgConfig.ts
+++ src/JSONFormMetadata/OrgConfig.ts
@@ -3,7 +3,7 @@ const Demo = {
     OrganizationUrl: "https://dev.azure.com/onegdcanalyticsdev",
     ProjectName: "Operational Framework",
     ProjectUrl: "https://dev.azure.com/onegdcanalyticsdev/Operational%20Framework",
-    FlowUrl:"https://prod-08.westus.logic.azure.com:443/workflows/91c0deef17a340d0995c7d23363ef673/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2iS29H79Emm_Vb69cRSkC78uIvLSkI2J9TRjhGbhl2A"
+    FlowUrl: "https://prod-08.westus.logic.azure.com:443/workflows/91c0deef17a340d0995c7d23363ef673/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2iS29H79Emm_Vb69cRSkC78uIvLSkI2J9TRjhGbhl2A"
 };
 
 const Test = {
@@ -11,15 +11,15 @@ const Test = {
     OrganizationUrl: "https://dev.azure.com/onegdcanalyticsdev",
     ProjectName: "Operational Framework Test",
     ProjectUrl: "https://dev.azure.com/onegdcanalyticsdev/Operational%20Framework%20Test",
-    FlowUrl:"https://prod-164.westus.logic.azure.com:443/workflows/6d316ee897dc48b5bd2d20095e3465ea/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=TkgKbzWgrsTGiVUfVnck2fccUMhmqM8lynRAABcdFWc"
+    FlowUrl: "https://prod-164.westus.logic.azure.com:443/workflows/6d316ee897dc48b5bd2d20095e3465ea/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=TkgKbzWgrsTGiVUfVnck2fccUMhmqM8lynRAABcdFWc"
 };
 
 const Prod = {
-    OrganizationName: "onegdcanalyticsdev",
-    OrganizationUrl: "https://dev.azure.com/onegdcanalyticsdev",
-    ProjectName: "Operational Framework Test",
-    ProjectUrl: "https://dev.azure.com/onegdcanalyticsdev/Operational%20Framework%20Test",
-    FlowUrl:""
+    OrganizationName: "Veena0200",
+    OrganizationUrl: "https://dev.azure.com/Veena0200",
+    ProjectName: "Teams App",
+    ProjectUrl: "https://dev.azure.com/Veena0200/Teams%20App",
+    FlowUrl: ""
 };
 
 export const OrganizationConfig = Test;
