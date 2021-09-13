a17eab01de568a68bc671bc653e2a17d1de65a04
veena.g <veena.g@technovert.com>
2021-09-13 12:16:26 +0530

    people picker get data, updated to emails from names

--- src/Services/SPService.ts
+++ src/Services/SPService.ts
@@ -57,9 +57,10 @@ export class SPService implements ISPService {
         filterStr = filterStr.concat(Area != "" ? ` and GDCEmailArea eq '${Area}'` : "");
         filterStr = filterStr.concat(PODCategory != "" ? ` and GDCEmailPODCategory eq '${PODCategory}'` : "");
 
-        var data = await this._localPnPSetup.web.lists.getByTitle('GDC Intake Form Notifications').items.filter(filterStr).select("Title,GDCEmailTo/Title,GDCEmailTeam,GDCEmailSubject,GDCEmailPODCategory,GDCEmailArea,GDCEmailBody,GDCEmailCc/Title").expand("GDCEmailTo,GDCEmailCc").getAll();
+        var data = await this._localPnPSetup.web.lists.getByTitle('GDC Intake Form Notifications').items.filter(filterStr).select("Title,GDCEmailTo/EMail,GDCEmailTeam,GDCEmailSubject,GDCEmailPODCategory,GDCEmailArea,GDCEmailBody,GDCEmailCc/EMail").expand("GDCEmailTo,GDCEmailCc").getAll();
         return data[0];
     }
+
     public async getOfficeUsersAlt(name): Promise<any> {
         const graphClient = await this._msGraphClientFactory.getClient();
 
@@ -105,18 +106,24 @@ export class SPService implements ISPService {
         var people: any = emaildata.GDCEmailTo;
         var to: any = [];
         people.forEach(email => {
-            to.push(email.Title);
+            to.push(email.EMail);
         });
-        var cc: any = [];
-        // let currentUser = await  this._localPnPSetup.web.currentUser()
-        await this._localPnPSetup.web.currentUser().then((data) => {
-            cc.push(data.Title);
-            formData.push({ "id": "CreatedBy", "value": data.Title });
-        });
-        var peopleCc: any = emaildata.GDCEmailCc != null ? emaildata.GDCEmailCc : [];
 
+        var peopleCc: any = emaildata.GDCEmailCc != null ? emaildata.GDCEmailCc : [];
+        var cc: any = [];
         peopleCc.forEach(element => {
-            cc.push(element.Title);
+            cc.push(element.EMail);
+        });
+
+        await this._localPnPSetup.web.currentUser().then((data) => {
+            let currentUserInTO = to.filter(t => t == data.Email);
+            let currentUserInCC = cc.filter(t => t == data.Email);
+            console.log("to", currentUserInTO, "cc", currentUserInCC);
+
+            if (currentUserInTO.length == 0 && currentUserInCC.length == 0) {
+                cc.push(data.Email);
+            }
+            formData.push({ "id": "CreatedBy", "value": data.Title });
         });
 
         let mailBodyStr = emaildata.GDCEmailBody;
@@ -150,7 +157,7 @@ export class SPService implements ISPService {
         });
 
         var emailProps: IEmailProperties = {
-            Body: mailBodyStr,
+            Body: `<div class=\"ExternalClassBF1CF651DAD7456D88FDABBF43DF8E4B\"><div style=\"font-family&#58;Calibri, Arial, Helvetica, sans-serif;background-color&#58;rgb(255, 255, 255);\"><div><span style=\"color&#58;black;\">​</span><span style=\"font-size&#58;10pt;\">Hi,</span></div></div><div style=\"font-family&#58;Calibri, Arial, Helvetica, sans-serif;background-color&#58;rgb(255, 255, 255);\"><div><p><br></p></div></div><div style=\"font-family&#58;Calibri, Arial, Helvetica, sans-serif;background-color&#58;rgb(255, 255, 255);\"><div><span style=\"font-size&#58;10pt;\">An intake request has been submitted to the</span><span style=\"font-size&#58;10pt;\">&#160;</span><span style=\"font-size&#58;10pt;\"><b>Operational Framework\\Targeting Enablement and Business Health\\AHR</b></span><span style=\"font-size&#58;10pt;\">&#160;backlog by</span><span style=\"font-size&#58;10pt;\">&#160;</span><span style=\"font-size&#58;10pt;\"><b>MOD Administrator.</b></span><span style=\"font-size&#58;10pt;\">&#160;</span><span style=\"font-size&#58;10pt;\">Use the link below to view the full user story and begin triage and prioritization.</span></div><p></p><ul><li><span style=\"background-color&#58;rgb(255, 255, 255);font-size&#58;10pt;display&#58;inline !important;\"><b>Link to User Story &#58;</b></span><span style=\"background-color&#58;rgb(255, 255, 255);display&#58;inline !important;\"><b><span style=\"font-size&#58;10pt;\">&#160;</span></b></span><span style=\"background-color&#58;rgb(255, 255, 255);font-size&#58;10pt;display&#58;inline !important;\"><a href='https://dev.azure.com/onegdcanalyticsdev/Operational%20Framework/_workitems/edit/1609'>Link</a></span><br></li><li><span style=\"font-size&#58;10pt;\"><b>Request Title &#58;</b></span><span style=\"font-size&#58;10pt;\">&#160;test 1118</span></li><li><span style=\"font-size&#58;10pt;\"><b>Need By Date &#58;</b></span><span style=\"font-size&#58;10pt;\">&#160;</span><br></li><li><span style=\"font-size&#58;10pt;\"><b>Requirement &#58;</b> <p><strong>Please supply the business/strategic question(s) you are seeking to answer with this request. (what questions are you trying to answer) </strong></p><p>sdfsdf</p><p>Microsoft Internet Explorer&amp;#39;s Enhanced Security Configuration is currently enabled on your environment. This enhanced level of security prevents our web integration experiences from displaying or performing correctly. To continue with your operation please disable this configuration or contact your administrator.</p><p><img src=\"https://dev.azure.com/onegdcanalyticsdev/_apis/wit/attachments/f2006f0f-31af-4058-9eb8-3a73ef8e7d25?fileName=image.png\"></p><p><br></p><p><strong>Objective: What will the output of this analysis, metrics, or report enable you to do/understand better or differently? </strong></p><p><br></p><p><br></p><p><strong>Hypothesis: If applicable, please provide a hypothesis statement to ground the analytics exploration and help provide the analyst with a point to work from. </strong></p><p><br></p><p><br></p><p><strong>How many users does this request affect? (myself, my area/subsidiary or all users) </strong></p><p><br></p><p><br></p><p><strong>What do we need to consider to support this request: data (fields, dimensions); time horizon (e.g. last 6, 12, or 18 months; trailing 12 months); what is the frequency you need this available for: 1x, weekly, monthly, quarterly </strong></p><p><br></p></span></li><li><span style=\"font-size&#58;10pt;\"><b>Impact &#58;</b> <p>dddasd</p></span></li><li><span style=\"font-size&#58;10pt;\"><b>Objective &#58; </b>sad</span></li><li><span style=\"font-size&#58;10pt;\"><b>Request Type &#58; </b></span></li><li><span style=\"font-size&#58;10pt;\"></span></li></ul><div><p><br></p><p><span style=\"font-size&#58;10pt;\">Thanks,</span></p><p><span style=\"font-size&#58;10pt;\">GDC Automation</span></p></div></div><br style=\"font-family&#58;Calibri, Arial, Helvetica, sans-serif;font-size&#58;14.6667px;background-color&#58;rgb(255, 255, 255);\"></div>`,
             Subject: mailSubjectStr,
             To: to,
             CC: cc
